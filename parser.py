b4bedd7b0bd02bc14f0047c921a33485d448ea59
ngonhi <ngonhi@grinnell.edu>
2019-05-11 00:57:53 -0500

    Fix parser

--- parser.py
+++ parser.py
@@ -27,49 +27,60 @@ class Parser():
     def parse_json(self):
         '''
         Parser for COCO dataset
+
         '''
         coco = self.coco
 
-        catIds = [1,2,3,4,5,6,7,8,9,10]
-        numClasses = len(catIds)
+        cat_ids = [1,2,3,4,5,6,7,8,9,10]
+        numClasses = len(cat_ids)
 
-        self.ground_truth = np.empty(shape = (0, numClasses + 4))
-        self.img_ids = []
+        self.ground_truth = []
+        self.img_ids = set()
 
         if self.resize_shape != None:
             width_ratio = self.resize_shape[0]
             height_ratio = self.resize_shape[1]
     
-        for id in catIds:
-            annIds = coco.getAnnIds(catIds = id)
-            # labels is 0,1 array, 1 marks the presence of class
-            labels = np.zeros(shape = (1, numClasses), dtype='int')
-            labels[0][id-1] = 1
-            anns = coco.loadAnns(annIds)
-            for ann in anns:
-                self.img_ids.append(ann['image_id']) 
-                img = coco.loadImgs(ann['image_id'])
-                width = img[0]['width']
-                height = img[0]['height']
+        # Get all images from chosen categories, prevent duplication
+        for cat_id in cat_ids:
+            images = coco.getImgIds(catIds=cat_id)
+            for img_id in images:
+                self.img_ids.add(img_id)
 
-                if self.resize_shape == None:
-                    width_ratio = width
-                    height_ratio = height
-                # ann['bbox'] = (xmin, ymin, w, h)
-                # bbox = (cx, cy, w, h)
-                # If we need to resize image, then divide by width/300 or height/300
-                bbox = np.zeros(shape=(1, 4), dtype='float')
-                bbox[0][0] = (float) (ann['bbox'][0] + ann['bbox'][2]) / 2 / width  \
-                                * (width_ratio / width) # Normalized
-                bbox[0][1] = (float) (ann['bbox'][1] + ann['bbox'][3]) / 2 / height \
-                                * (height_ratio / height) # Normalized
-                bbox[0][2] = (float) (ann['bbox'][2]) / width * (width_ratio / width)
-                bbox[0][3] = (float) (ann['bbox'][3]) / height * (height_ratio / height)
-    
-                # Create a ground_truth item
-                gt = np.append(labels, bbox, axis = 1)
-                self.ground_truth = np.append(self.ground_truth, gt, axis = 0) #Shape(#gt, numClasses + 4)
+        for img_id in self.img_ids: 
+            batch = np.empty(shape=(0, numClasses + 4), dtype='float')
+            img = coco.loadImgs(img_id)
 
+            # Get image dimension
+            width = img[0]['width']
+            height = img[0]['height']
+
+            if self.resize_shape == None:
+                width_ratio = width
+                height_ratio = height
+
+            for cat_id in cat_ids:
+                # labels is 0,1 array, 1 marks the presence of class
+                labels = np.zeros(shape = (1, numClasses), dtype='int')
+                labels[0][cat_id-1] = 1                
+
+                ann_ids = coco.getAnnIds(imgIds=img_id, catIds=cat_id)
+
+                # Loop through annotations for each image
+                for ann_id in ann_ids:
+                    ann = coco.loadAnns(ann_id)
+                    #print(ann[0]['bbox'])
+                    # ann_id['bbox'] = (xmin, ymin, w, h)
+                    bbox = np.zeros(shape=(1,4), dtype='float')
+                    bbox[0][0] = (float) (ann[0]['bbox'][0] + ann[0]['bbox'][2]) / 2 / width  * (width_ratio / width) # Normalized
+                    bbox[0][1] = (float) (ann[0]['bbox'][1] + ann[0]['bbox'][3]) / 2 / height * (height_ratio / height) # Normalized
+                    bbox[0][2] = (float) (ann[0]['bbox'][2]) / width * (width_ratio / width)
+                    bbox[0][3] = (float) (ann[0]['bbox'][3]) / height * (height_ratio / height)
+
+                    # Create a ground_truth item
+                    gt = np.append(labels, bbox, axis=1)
+                    batch = np.append(batch, gt, axis=0)
+            self.ground_truth.append(batch)
         return self.ground_truth
 
 
@@ -81,14 +92,14 @@ class Parser():
         # Open file for writing (overwrite mode)
         # Change path when running
         path = os.getcwd()
+
         if(fileName==None): 
             self.fileName = "imagePaths.txt"
         else: 
             self.fileName = fileName + ".txt"
 
-        path = path + self.fileName
-
-        f= open(path,"w")
+        path = path + '/' + self.fileName
+        f= open(path,"w+")
 
         for imgId in self.img_ids:
             im_path = '{}/{}/{:012}.jpg\n'.format(self.data_dir, self.data_type, imgId)
@@ -98,7 +109,6 @@ class Parser():
 
     def load_training_imgs(self):
         path = os.getcwd()
-
         path = path + self.fileName
 
         im_collection = []
@@ -130,11 +140,12 @@ class Parser():
 
 
 def main(): 
-    # data_dir = "/Users/ngophuongnhi/Desktop/csc262proj/cocoapi"
-    data_dir = "/Users/tranle/mscoco"
+    data_dir = "/Users/ngophuongnhi/Desktop/csc262proj/cocoapi"
+    #data_dir = "/Users/tranle/mscoco"
     data_type = "val2017"
     p = Parser(data_dir, data_type)
     gt = p.parse_json()
+    f = open("/Users/ngophuongnhi/Desktop/imagePaths.txt", "w+")
 
     p.load_img_paths()
 
@@ -143,7 +154,6 @@ def main():
     bbox_coord = gt[:10][-4:]
     print(type(im_collection))
     print(len(im_collection))
-    print(gt.shape)
     images = tf.constant(im_collection)
     bbox_coord[:,2] += bbox_coord[:, 0]
     bbox_coord[:, 3] += bbox_coord[:, 1]
@@ -151,6 +161,7 @@ def main():
     coords = tf.constant(bbox_coord)
 
     plt.imshow(images[0])
+
 
 
 
